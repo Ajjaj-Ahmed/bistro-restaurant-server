9f6dcd50c633d816c5af5ae68bef9369078a5c76
Ajjaj Ahmed <ajjajahmedasif@gmail.com>
2025-03-18 22:19:15 +0600

    users collection get post

--- index.js
+++ index.js
@@ -32,8 +32,19 @@ async function run() {
     const cartCollection = client.db("bistroDb").collection("carts");
 
     // user related api
+    app.get('/users', async (req, res)=>{
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post('/users', async (req,res)=>{
       const user = req.body;
+      // insert email if user does not exit in db
+      const query ={email : user.email}
+      const existingUser = await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({message: 'user already exits', insertedId: null})
+      }
       const result = await userCollection.insertOne(user);
       res.send(result)
     })
