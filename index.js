458eb604e8f22809eb3970caa78e8027aa6aead7
Ajjaj Ahmed <ajjajahmedasif@gmail.com>
2025-04-06 21:45:16 +0600

    stripe secret

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-require('dotenv').config()
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+// STRIPE_SECRET_KEY=sk_test_51RAmG3IzAIJkATc0MJpRgiLvO45WeUpQbasHg7nR0QPR90QpGKAgPVhEKD16gg4HX84rZecsXvdzuMyb9Fl1EML000NXWx61NE
+
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -144,7 +147,7 @@ async function run() {
       res.send(result);
     })
 
-    app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
+    app.delete('menu/:id', verifyToken, verifyAdmin, async(req, res)=>{
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await menuCollection.deleteOne(query);
@@ -177,6 +180,25 @@ async function run() {
       const result = await cartCollection.deleteOne(query);
       res.send(result)
     })
+    // payment 
+    app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      console.log(amount, 'amount inside the intent')
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    });
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
